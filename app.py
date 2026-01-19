85441a62a1a3e863719855479e848bee841d4477
glitch-glitz <fullstack.hub.devx@gmail.com>
2026-01-19 17:17:46 +0300

    Updated app.py

--- app.py
+++ app.py
@@ -2,23 +2,16 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
+from models import db
 
 app = Flask(__name__)
 
-# setup cors
 CORS(app)
-
-# setup flask-restful
 api = Api(app)
 
-# configure database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///note.db"
-app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# initialize db
-db = SQLAlchemy(app)
+db.init_app(app)
 
-# initialize migrate
 migrate = Migrate(app, db)
